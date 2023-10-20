374082e0f7861a01c1a692857c2462faafbdbe14
Yash Patel <pyash031@gmail.com>
2023-10-20 15:57:47 -0400

    hi

---
+++ main1.py
@@ -0,0 +1,38 @@
+# Encode the password
+def encoder(message):
+    result = ''
+    for digit in message:
+        new_digit = str((int(digit) + 3) % 10)
+        result += new_digit
+    return result
+
+
+# Decode the password
+def decoder(message):
+    pass
+
+
+# Main loop which shows Menu
+def main():
+    encode_value = 0
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        choice = input("Enter your option: ")
+        if choice == "1":
+            value = input("Please enter your password to encode: ")
+            encode_value = encoder(value)
+            print("Your password has been encoded and stored!")
+        elif choice == "2":
+            decode_value = decoder(encode_value)
+            print(f"The encoded password is {encode_value}, and the original password is {decode_value}.")
+        elif choice == "3":
+            break
+        print()
+
+
+if __name__ == "__main__":
+    main()
